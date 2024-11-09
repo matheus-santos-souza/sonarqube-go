82435939f18616cab60d8b117210380a2dc0778c
matheus-santos-souza <matheus.santos.souza2020@outlook.com>
2024-11-09 11:31:42 -0400

    test: :white_check_mark: add unity tests

--- sum_test.go
+++ sum_test.go
@@ -25,3 +25,11 @@ func TestTimes(t *testing.T) {
 		t.Error("The result must be 20")
 	}
 }
+
+func TestSumX(t *testing.T) {
+	result := SumX(10, 10)
+
+	if result != 30 {
+		t.Error("The result must be 20")
+	}
+}
